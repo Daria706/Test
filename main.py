f878887d7389832d3047ad042df31e006ce3872a
Daria706 <Ilicheva-DM2020@sgugit.ru>
2021-09-16 13:12:26 +0700

    commit 2

--- main.py
+++ main.py
@@ -1,16 +1,8 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, I am student')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+a= input("Введите первое число ")
+b= input("Введите первое число ")
+if a>b:
+    print ("Первое число больше")
+elif a<b:
+    print ("Второе число больше")
+else:
+    print ("Числа равны")
